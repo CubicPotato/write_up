877f02c1c938aaff7690a369006a0bcf40f16ead
CubicPotato <paulledarue@gmail.com>
2026-09-21 21:03:11 +0200

    fin tuto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,26 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://cubic-blog.netlify.app/",
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+    site: "https://cubic-blog.netlify.app/",
+    integrations: [starlight({
+        title: 'My Docs',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                items: [{ autogenerate: { directory: 'reference' } }],
+            },
+        ],
+		}), preact()],
 });
